7ef1c720ea0006b6af2ea49aecc5db700d97cc49
konflic <konflic@mail.ru>
2022-07-09 00:08:02 +0300

    Move chat to guiless format

--- backend/client.py
+++ backend/client.py
@@ -4,45 +4,44 @@ import threading
 
 class Client:
 
-    def __init__(self, ip, port, name="", client_gui=None):
+    def __init__(self, ip, port, name=""):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, int(port)))
-        self.client_gui = client_gui
         if not isinstance(name, str) or name == "":
             raise Exception(f"Empty or not string username not allowed! Got: {name}")
         self.connection.send(name.encode("utf-8"))
+        self.start_client()
 
-    def __decoder(self, message):
+    def _decode(self, message):
         if isinstance(message, bytes):
             return message.decode("utf-8")
         elif isinstance(message, str):
             return message.encode("utf-8")
 
-    def start(self):
+    def start_client(self):
+        print("=> Start accepting messages from server")
         self.conn = threading.Thread(target=self.receiving_messages)
         self.conn.start()
-        self.client_gui.set_connected()
-
-    def leave(self):
-        self.connection.close()
-        self.client = None
-        self.client_gui.set_disconnected()
+        print("=> Start sending messages to server")
+        self.conn = threading.Thread(target=self.sending_messages)
+        self.conn.start()
 
     def receiving_messages(self):
         while True:
-            from_server = self.connection.recv(4096)
-            if not from_server: break
-            self.client_gui.add_message(self.__decoder(from_server))
-        self.client_gui.set_disconnected()
+            data_from_server = self.connection.recv(4096)
+            if not data_from_server:
+                break
+            print(self._decode(data_from_server))
 
-    def send_message(self):
-        message_text = self.client_gui.message_box.text()
-        self.connection.send(self.__decoder(message_text))
-        self.client_gui.message_box.clear()
+    def sending_messages(self):
+        while True:
+            message = input()
+            if message == "exit":
+                self.connection.close()
+            self.connection.send(self._decode(message=message))
 
 
 if __name__ == "__main__":
-    host = input("HOST: ")
-    port = input("PORT: ")
+    host, port = input("HOST:PORT ").split(":")
     name = input("Username: ")
     client = Client(host, int(port), name)
